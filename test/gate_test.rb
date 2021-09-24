eb41b8057a5b777d12854d2ddbb1230e7fd72c1f
masanarih0ri <masanarihoridevelop@gmail.com>
2021-09-24 18:19:31 +0900

    add ticket and gate

--- test/gate_test.rb
+++ test/gate_test.rb
@@ -3,11 +3,34 @@ require './lib/gate'
 require './lib/ticket'
 
 class GateTest < Minitest::Test
-  def test_gate
-    umeda = Gate.new(:umeda)
-    juso = Gate.new(:juso)
+  # Minitestにはsetupメソッドがあり、毎回これが呼ばれるようになる
+  def setup
+    @umeda = Gate.new(:umeda)
+    @juso = Gate.new(:juso)
+    @mikuni = Gate.new(:mikuni)
+  end
+  
+  def test_umeda_to_juso
     ticket = Ticket.new(150)
-    umeda.enter(ticket)
-    assert juso.exit(ticket)
+    @umeda.enter(ticket)
+    assert @juso.exit(ticket)
+  end
+  
+  def test_umeda_to_minkuni_when_fare_is_not_enough
+    ticket = Ticket.new(150)
+    @umeda.enter(ticket)
+    refute @mikuni.exit(ticket)
+  end
+  
+  def test_umeda_to_minkuni_when_fare_is_enough
+    ticket = Ticket.new(190)
+    @umeda.enter(ticket)
+    assert @mikuni.exit(ticket)
+  end
+  
+  def test_juso_to_minkuni
+    ticket = Ticket.new(150)
+    @juso.enter(ticket)
+    assert @mikuni.exit(ticket)
   end
 end
